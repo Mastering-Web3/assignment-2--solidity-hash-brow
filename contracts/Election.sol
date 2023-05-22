c1ea8ea2a978a8c01b62367faa66bd249dd19cd0
hash-brow <div4804@gmail.com>
2023-05-23 03:46:28 +0530

    upd Election.sol with TODOs done

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,89 +2,112 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Election {
-    // This is a type for a single voter.
-    struct Voter {
-        bool registered; // if true, this voter is eligible to vote
-        bool voted; // if true, this voter already voted
-        uint256 vote; // index of the voted candidate
-    }
+	// This is a type for a single voter.
+	struct Voter {
+		bool registered; // if true, this voter is eligible to vote
+		bool voted; // if true, this voter already voted
+		uint256 vote; // index of the voted candidate
+	}
 
-    // This is a type for a single candidate.
-    struct Candidate {
-        string name; // short name (up to 32 bytes)
-        uint256 votes; // total number of votes
-    }
+	// This is a type for a single candidate.
+	struct Candidate {
+		string name; // short name (up to 32 bytes)
+		uint256 votes; // total number of votes
+	}
 
-    // This is the leader (creator) of the election.
-    address public leader;
+	// This is the leader (creator) of the election.
+	address public leader;
 
-    // This declares a state variable that
-    // maps a `Voter` to each possible address.
-    mapping(address => Voter) public voters;
+	// This declares a state variable that
+	// maps a `Voter` to each possible address.
+	mapping(address => Voter) public voters;
 
-    // A dynamically-sized array of `Candidate` structs.
-    Candidate[] public candidates;
+	// A dynamically-sized array of `Candidate` structs.
+	Candidate[] public candidates;
 
-    /// Create a new election to choose one of `_candidateNames`.
-    constructor(string[] memory _candidateNames) {
-        leader = msg.sender;
-        voters[leader].registered = true;
+	/// Create a new election to choose one of `_candidateNames`.
+	constructor(string[] memory _candidateNames) {
+		leader = msg.sender;
+		voters[leader].registered = true;
 
-        // For each provided candidate name, create
-        // a new `Candidate` and add it to the end
-        // of the `voters` array.
-        for (uint256 i = 0; i < _candidateNames.length; i++) {
-            // `Candidate({...})` creates a temporary
-            // Candidate object and `candidates.push(...)`
-            // appends it to the end of `candidates`.
-            candidates.push(Candidate({name: _candidateNames[i], votes: 0}));
-        }
-    }
+		// For each provided candidate name, create
+		// a new `Candidate` and add it to the end
+		// of the `voters` array.
+		for (uint256 i = 0; i < _candidateNames.length; i++) {
+			// `Candidate({...})` creates a temporary
+			// Candidate object and `candidates.push(...)`
+			// appends it to the end of `candidates`.
+			candidates.push(Candidate({name: _candidateNames[i], votes: 0}));
+		}
+	}
 
-    // Grants the right to vote on this ballot to `voter`.
-    // Can only be called by `leader`.
-    function registerVoter(address voter) external {
-        require(
-            msg.sender == leader,
-            "Only the election leader can grant voting rights."
-        );
-        require(!voters[voter].voted, "The voter already voted.");
-        require(!voters[voter].registered, "Voter is already registered.");
-        // TODO: Register a voter with the given address by setting
-        // the `registered` field of the corresponding Voter to true.
-    }
+	// Grants the right to vote on this ballot to `voter`.
+	// Can only be called by `leader`.
+	function registerVoter(address voter) external {
+		require(
+			msg.sender == leader,
+			"Only the election leader can grant voting rights."
+		);
+		require(!voters[voter].voted, "The voter already voted.");
+		require(!voters[voter].registered, "Voter is already registered.");
+		// TODO: Register a voter with the given address by setting
+		// the `registered` field of the corresponding Voter to true.
 
-    /// Give your vote
-    /// to candidate `candidates[_candidate].name`.
-    function castVote(uint256 _candidate) external {
-        Voter storage voter = voters[msg.sender];
-        require(voter.registered, "Voter is not registered.");
-        require(!voter.voted, "Already voted.");
+		voters[voter].registered = true;
+	}
 
-        // TODO: Cast a vote for the given _candidate.
-    }
+	/// Give your vote
+	/// to candidate `candidates[_candidate].name`.
+	function castVote(uint256 _candidate) external {
+		Voter storage voter = voters[msg.sender];
+		require(voter.registered, "Voter is not registered.");
+		require(!voter.voted, "Already voted.");
 
-    // Determine the winning candidate.
-    function winningCandidate()
-        public
-        view
-        returns (uint256 winningCandidate_)
-    {
-        // TODO: Iterate through the candidates array
-        // and determine which candidate got the most votes.
-        // Return the index of the winner in `winningCandidate_`
-        // Error and revert if no votes have been cast yet.
-    }
+		// TODO: Cast a vote for the given _candidate.
+		candidates[_candidate].votes = candidates[_candidate].votes + 1;
+		voter.voted = true;
+	}
 
-    // Calls winningCandidate() function to get the index
-    // of the winner and then returns the name of the winner
-    function winningCandidateName()
-        external
-        view
-        returns (string memory winnerName_)
-    {
-        // TODO: Return the name of the 
-        // winner in `winnerName_` using the
-        // candidates array and the winningCandidate() function.
-    }
+	// Determine the winning candidate.
+	function winningCandidate()
+		public
+		view
+		returns (uint256 winningCandidate_)
+	{
+		// TODO: Iterate through the candidates array
+		// and determine which candidate got the most votes.
+		// Return the index of the winner in `winningCandidate_`
+		// Error and revert if no votes have been cast yet.
+
+		uint256 winner;
+		uint256 maxVotes = 0;
+		bool votesCasted = false;
+
+		for (uint256 i = 0; i < candidates.length; i++){
+			if(candidates[i].votes > 0)
+				votesCasted = true;
+
+			if(candidates[i].votes > maxVotes){
+				maxVotes = candidates[i].votes;
+				winner = i;
+			}
+		}
+		require(votesCasted, "No votes have been cast.");
+
+		return winner;
+	}
+
+	// Calls winningCandidate() function to get the index
+	// of the winner and then returns the name of the winner
+	function winningCandidateName()
+		external
+		view
+		returns (string memory winnerName_)
+	{
+		// TODO: Return the name of the 
+		// winner in `winnerName_` using the
+		// candidates array and the winningCandidate() function.
+
+		return candidates[winningCandidate()].name;
+	}
 }
